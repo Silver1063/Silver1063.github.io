b75b642d4da2a55a723e41c572b2add4f667cd55
Silver1063 <brickbrickerson1063@gmail.com>
2020-06-08 19:26:06 -0700

    Delete mycode.js

--- source/Hour2/mycode.js
+++
@@ -1,5 +0,0 @@
-var cTemp = 100;
-var hTemp = ((cTemp * 9) / 5) + 32;
-
-document.write("Tempature in Celsius: " + cTemp + " degrees<br/>");
-document.write("Tempature in Fahrenheit: " + hTemp + " degrees");
