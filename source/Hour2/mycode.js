0672ecef242ad6b6eb9b4590a448c52242b67164
Silver1063 <brickbrickerson1063@gmail.com>
2020-06-08 19:25:46 -0700

    Week 1 complete

---
+++ source/Hour2/mycode.js
@@ -0,0 +1,5 @@
+var cTemp = 100;
+var hTemp = ((cTemp * 9) / 5) + 32;
+
+document.write("Tempature in Celsius: " + cTemp + " degrees<br/>");
+document.write("Tempature in Fahrenheit: " + hTemp + " degrees");
