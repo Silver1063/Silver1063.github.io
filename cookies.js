d350197497e320e10b1d914d90e7c139ac8d3f01
Silver1063 <brickbrickerson1063@gmail.com>
2020-07-03 11:00:17 -0700

    updates

---
+++ cookies.js
@@ -0,0 +1,36 @@
+function createCookie(name, value, days, path, domain, secure)
+{
+    if(days)
+    {
+        var date = new Date();
+        date.setTime( date.getTime() + (days*24*60*60*1000));
+        var expires = date.toGMTString();
+    }
+    else var expires = "";
+    cookieString = name + "=" + escape (value);
+    if (expires) cookieString += "; expires=" + expires;
+    if (path) cookieString += "; path=" + path;
+    if (domain) cookieString += "; domain=" + domain;
+    if (secure) cookieString += "; secure=" + secure;
+    document.cookie = cookieString;
+}
+
+function getCookie(name)
+{
+    var nameEquals = name + "=";
+    var crumbs =  document.cookie.split(";");
+    for(var i = 0; i< crumbs.length; i++)
+    {
+        var crumb = crumbs[i].trim();
+        if(crumb.indexOf(nameEquals) == 0)
+        {
+            return unescape(crumb.substring(nameEquals.length, crumb.length));
+        }
+    }
+    return null;
+}
+
+function deleteCookie(name)
+{
+    createCookie(name,"",-1);
+}
